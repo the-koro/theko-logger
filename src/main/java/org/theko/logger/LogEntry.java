889b8f0a59e72bd3315f5e1600f8ad13728a19f2
koroche-sanya <krasnobaev.sasha11@gmail.com>
2024-12-28 23:31:01 +0200

    Added method to export log entry as json object

--- src/main/java/org/theko/logger/LogEntry.java
+++ src/main/java/org/theko/logger/LogEntry.java
@@ -1,5 +1,7 @@
 package org.theko.logger;
 
+import org.json.JSONObject;
+
 /**
  * Represents a single log entry containing information about the log level, timestamp, 
  * caller information, and the actual log message.
@@ -93,6 +95,38 @@ public class LogEntry {
 
     public String getMessage() {
         return message;
+    }
+
+    /**
+     * Converts the log entry to a JSON object representation.
+     * 
+     * @return A JSONObject containing the log entry details.
+     */
+    public JSONObject getJSONObject() {
+        JSONObject json = new JSONObject();
+        
+        // Adding log level, timestamp, and message to JSON
+        json.put("level", level.name());
+        json.put("time", time);
+        json.put("message", message);
+        
+        // If caller information is available, add those details as well
+        if (caller != null) {
+            JSONObject callerJson = new JSONObject();
+            callerJson.put("className", caller.getClassName());
+            callerJson.put("methodName", caller.getMethodName());
+            callerJson.put("nativeMethod", caller.isNativeMethod());
+            callerJson.put("moduleName", caller.getModuleName());
+            callerJson.put("moduleVersion", caller.getModuleVersion());
+            callerJson.put("classLoaderName", caller.getClassLoaderName());
+            callerJson.put("threadName", caller.getThreadName());
+            callerJson.put("fileName", caller.getFileName());
+            callerJson.put("lineNumber", caller.getLineNumber());
+            
+            // Add caller info as a nested object in the main JSON object
+            json.put("caller", callerJson);
+        }
+        return json;
     }
 
     /**
