a062849139fa00e41d72a5ec404cbde5856c6454
koroche-sanya <krasnobaev.sasha11@gmail.com>
2025-01-30 23:30:02 +0200

    Add consumer when log entry processed. Add support of tagged logs

--- src/main/java/org/theko/logger/AsyncLogger.java
+++ src/main/java/org/theko/logger/AsyncLogger.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * AsyncLogger extends DefaultLogger to handle logging asynchronously.
@@ -13,6 +14,11 @@ import java.util.concurrent.TimeUnit;
 public class AsyncLogger extends DefaultLogger {
     private final BlockingQueue<LogEntry> logQueue;
     private final ExecutorService executor;
+
+    /**
+     * Consumer that will be triggered whenever a log entry is processed.
+     */
+    private Consumer<LogEntry> onLogProcessed;
 
     /**
      * Constructs an AsyncLogger with a specified LoggerOutput and thread pool size.
@@ -27,7 +33,7 @@ public class AsyncLogger extends DefaultLogger {
     }
 
     /**
-     * Constructs an AsyncLogger with thread pool size.
+     * Constructs an AsyncLogger with the default LoggerOutput and thread pool size.
      */
     public AsyncLogger() {
         super();
@@ -47,8 +53,8 @@ public class AsyncLogger extends DefaultLogger {
                     if (loggerOutput != null) {
                         loggerOutput.processToOut(log);
                     }
-                    if (onLogCreated != null) {
-                        onLogCreated.accept(log);
+                    if (onLogProcessed != null) {
+                        onLogProcessed.accept(log);
                     }
                 }
             } catch (InterruptedException e) {
@@ -57,6 +63,14 @@ public class AsyncLogger extends DefaultLogger {
         });
     }
 
+    /**
+     * Logs a message at a specified log level asynchronously.
+     *
+     * @param level The log level at which the message should be logged.
+     * @param message The message to be logged.
+     * @param stackTraceOffset The stack trace offset to identify the caller info.
+     * @return The created LogEntry.
+     */
     @Override
     public LogEntry log(LogLevel level, String message, int stackTraceOffset) {
         LogEntry log = createLogEntry(level, message, stackTraceOffset);
@@ -64,14 +78,35 @@ public class AsyncLogger extends DefaultLogger {
         return log;
     }
 
+    /**
+     * Logs a message at a specified log level asynchronously, with tags.
+     *
+     * @param level The log level at which the message should be logged.
+     * @param message The message to be logged.
+     * @param tags The tags associated with the log.
+     * @param stackTraceOffset The stack trace offset to identify the caller info.
+     * @return The created LogEntry.
+     */
+    @Override
+    public LogEntry log(LogLevel level, String message, String[] tags, int stackTraceOffset) {
+        LogEntry log = createLogEntry(level, message, tags, stackTraceOffset);
+        logQueue.offer(log); // Add log to the processing queue
+        return log;
+    }
+
+    /**
+     * Drains all logs from the queue and processes them synchronously.
+     *
+     * @throws InterruptedException If the thread is interrupted while waiting.
+     */
     public void drain() throws InterruptedException {
         while (!logQueue.isEmpty()) {
             LogEntry log = logQueue.take(); // Block until a log is available
             if (loggerOutput != null) {
                 loggerOutput.processToOut(log);
             }
-            if (onLogCreated != null) {
-                onLogCreated.accept(log);
+            if (onLogProcessed != null) {
+                onLogProcessed.accept(log);
             }
         }
     }
@@ -89,5 +124,14 @@ public class AsyncLogger extends DefaultLogger {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
+    }
+
+    /**
+     * Sets the consumer that will be triggered when a log entry is processed.
+     *
+     * @param onLogProcessed The consumer to handle processed log entries.
+     */
+    public void setOnLogProcessed(Consumer<LogEntry> onLogProcessed) {
+        this.onLogProcessed = onLogProcessed;
     }
 }
